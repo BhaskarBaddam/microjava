66bc11e138b88305aea8fe4fdf092c84a451e3c6
BhaskarBaddam <121882476+BhaskarBaddam@users.noreply.github.com>
2025-07-31 11:54:52 +0530

    Update InventoryServiceApplication.java

--- service-inventory/src/main/java/com/example/inventory/InventoryServiceApplication.java
+++ service-inventory/src/main/java/com/example/inventory/InventoryServiceApplication.java
@@ -1,1 +1,21 @@
+package com.example.inventory;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class InventoryServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(InventoryServiceApplication.class, args);
+    }
+}
+
+@RestController
+class InventoryController {
+    @GetMapping("/inventory")
+    public String getInventory() {
+        return "List of inventory items";
+    }
+}
