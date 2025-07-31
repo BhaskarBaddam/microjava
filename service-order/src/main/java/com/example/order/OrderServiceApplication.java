90b81179e305c83c0a4df7f02e3cae961031b7dd
BhaskarBaddam <121882476+BhaskarBaddam@users.noreply.github.com>
2025-07-31 11:50:56 +0530

    Create OrderServiceApplication.java

---
+++ service-order/src/main/java/com/example/order/OrderServiceApplication.java
@@ -0,0 +1,21 @@
+package com.example.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class OrderServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderServiceApplication.class, args);
+    }
+}
+
+@RestController
+class OrderController {
+    @GetMapping("/orders")
+    public String getOrders() {
+        return "List of orders";
+    }
+}
